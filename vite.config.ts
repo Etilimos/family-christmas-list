58382d61bac3fd71db209b2cedc637a863497a81
Etilimos <etilimos@hotmail.fr>
2024-10-07 21:21:33 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/family-christmas-list/', // This line is important for GitHub Pages
-})
